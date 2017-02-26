30aa296cba52fa0226c25fe0a3fc94f9cd96c814
m3ss0r3m <m3ss0r3m@gmail.com>
2017-02-26 17:54:39 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ try:
 except ImportError:
     from distutils.core import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = "syntaxdb",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "lhat-messorem",
     author_email = "lhat.messorem@gmail.com",
     description = ("A small Python library for accessing the SyntaxDB API"),
@@ -17,6 +20,7 @@ setup(
     keywords = "syntaxdb",
     url = "https://github.com/lhat-messorem/syntax_db",
     packages=['syntaxdb'],
+    long_description=read('README.rst'),
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python",
